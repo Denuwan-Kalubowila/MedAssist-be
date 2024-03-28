d500e9e81191aafaa659e19f6f7a4c83c0a98534
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-03-28 11:05:35 +0530

    basic changes

--- midassist/core/urls.py
+++ midassist/core/urls.py
@@ -1,3 +1,4 @@
+"""This module providing  url array of MedAssist"""
 from django.urls import path
 from .views import your_view
 from . import views
@@ -5,9 +6,9 @@ from . import views
 
 urlpatterns = [
     path('your-endpoint/', your_view, name='your_view'),
-    path('testDB/', views.testdb),
-    path('testDB/<str:pk>/', views.testdb_one),
-    path('createValue/', views.create_value),
-    path('testDB/<str:pk>/update', views.update_value),
-    path('testDB/<str:pk>/delete', views.delete_value),
+    path('test-database/', views.testdb),
+    path('test-database/<str:pk>/', views.testdb_one),
+    path('create-value/', views.create_value),
+    path('test-database/<str:pk>/update', views.update_value),
+    path('test-database/<str:pk>/delete', views.delete_value),
 ]
