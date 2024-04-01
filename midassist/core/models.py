4520f857c3dc39ec4021a6877483fd4b4ea169ab
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-04-01 22:56:56 +0530

    add user model

--- midassist/core/models.py
+++ midassist/core/models.py
@@ -1,13 +1,45 @@
+
+"""
+This module represent the models of MedAssist
+"""
 from django.db import models
 
 class TestDB(models.Model):
     """Create your models here."""
     body = models.TextField()
     update = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.body[0:50]
 
     class Meta:
+        """
+        Test Meta Data
+        """
         ordering = ['-update']
+
+
+class Member(models.Model):
+    """
+    Member Model Fields
+    """
+    first_name=models.TextField()
+    last_name=models.TextField()
+    age=models.IntegerField()
+    email=models.EmailField()
+    contact_number=models.CharField(max_length=10)
+    password=models.TextField()
+    is_admin=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    created=models.DateTimeField(auto_now=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        """
+        Member Meta Data
+        """
+        ordering=['created']
