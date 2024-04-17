56a31202c276384a441cd933721e37e8576bf8ef
D. Binara Kaveesha Weerasekara <dbkaveesha@gmail.com>
2024-04-17 16:21:10 +0530

    add doctor model

--- midassist/core/urls.py
+++ midassist/core/urls.py
@@ -1,11 +1,13 @@
 """This module providing  url array of MedAssist"""
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('sign-up/', views.signup_view,name="signup"),
+    path('sign-up/', views.signup_view, name="signup"),
     path('sign-in/', views.login_view),
     path('log-out/', views.logout_view),
     path('upload_image/', views.post_image),
+    path('doctors/', views.doctors_view, name="doctors"),
 
 ]
--- midassist/midassist/settings.py
+++ midassist/midassist/settings.py
@@ -59,8 +59,8 @@ WSGI_APPLICATION = 'midassist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':os.getenv('DATABASE_ENGINE'),
-        'NAME': os.getenv('DATABASE_NAME'),
+        'ENGINE': 'djongo',
+        'NAME': 'MidAssist',
     }
 }
 
