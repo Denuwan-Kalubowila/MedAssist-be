ca5772df1cda67ab19874fa62eeaab3db7918b27
D. Binara Kaveesha Weerasekara <dbkaveesha@gmail.com>
2024-05-10 22:12:32 +0530

    fix bugs in pdf uploder

--- midassist/core/urls.py
+++ midassist/core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('upload_image/', views.post_image),
     path('doctors/', views.doctors_view, name="doctors"),
     path('users/', views.user_details, name="users"),
-    path('chat/', views.chat, name="chat"),
+    path('upload_pdf/', views.post_pdf),
+    # path('chat/', views.chat, name="chat"),
 
 ]
