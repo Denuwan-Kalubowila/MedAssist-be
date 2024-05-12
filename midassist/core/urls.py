8423fe00d19439eda4a5e5c7d82538c0c0e15dfc
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-05-12 20:54:57 +0530

    uncomment chat url

--- midassist/core/urls.py
+++ midassist/core/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('doctors/', views.doctors_view, name="doctors"),
     path('users/', views.user_details, name="users"),
     path('upload_pdf/', views.post_pdf),
-    # path('chat/', views.chat, name="chat"),
+    path('chat/', views.chat, name="chat"),
 
 ]
