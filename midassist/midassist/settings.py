d7e13387330e04c17adc3789694acd5599f969bc
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-08-18 20:51:13 +0530

    venv

--- midassist/midassist/settings.py
+++ midassist/midassist/settings.py
@@ -21,9 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+  
     'rest_framework',
     'rest_framework.authtoken',
     "corsheaders",
+   
 ]
 
 MIDDLEWARE = [
@@ -35,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'midassist.urls'
