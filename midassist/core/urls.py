4f298764276655b082e5cc7cf524d77dfaca5c69
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-08-20 14:53:31 +0530

    doctor end-points

--- midassist/core/urls.py
+++ midassist/core/urls.py
@@ -5,13 +5,13 @@ from . import views
 
 urlpatterns = [
     path('sign-up/', views.signup_view, name="signup"),
-    path('doctors/', views.post_doctor,name="post_doctor"),
-    path('doctors/', views.doctors_view, name="doctors"),
-    path('doctors/reviews/', views.get_doctotrs_by_review, name="doctor_reviews"),
+    path('doctor/', views.post_doctor,name="post_doctor"),
+    path('doctor/', views.doctors_view, name="doctors"),
+    path('doctors/review', views.get_doctotrs_by_review, name="doctor_reviews"),
     path('sign-in/', views.login_view),
     path('log-out/', views.logout_view),
     path('upload_image/', views.post_image),
     path('users/', views.user_details, name="users"),
     path('upload_pdf/', views.post_pdf),
-    path('chat/', views.chat, name="chat"),
+    path('medchat/', views.chat, name="chat"),
 ]
